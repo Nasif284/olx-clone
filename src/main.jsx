fa8f38e1a728576e70bf511f63087b1f664b9c56
Muhammad Nasif <nasifhima@gmail.com>
2026-01-02 18:05:15 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './Redux/store.js'
-import ErrorBoundary from './Components/ErrorBoundary.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store.js";
+import ErrorBoundary from "./Components/ErrorBoundary.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-        <StrictMode>
+    <StrictMode>
       <Provider store={store}>
         <ErrorBoundary>
-            <App />
-          </ErrorBoundary>
-          </Provider>
-        </StrictMode>
+          <App />
+        </ErrorBoundary>
+      </Provider>
+    </StrictMode>
   </BrowserRouter>
 );
